a6968d5b84e102943e6664429c2b94ed7b476ce5
HanaYoriDango <HanaYoriDango@DESKTOP-JFUIT64>
2025-11-10 18:49:09 +0700

    Add project files.

---
+++ TheImmature/main.cpp
@@ -0,0 +1,15 @@
+﻿
+
+// Настройки: 
+//linker::system::subsystem  - Windows(/ SUBSYSTEM:WINDOWS) 
+//configuration::advanced::character set - not set 
+//linker::input::additional dependensies Msimg32.lib; Winmm.lib
+
+
+
+
+
+
+
+
+
