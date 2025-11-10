f2785b15006a40c86b32087bb4be17b858f65643
HanaYoriDango <HanaYoriDango@DESKTOP-JFUIT64>
2025-11-10 19:43:06 +0700

    AddMain

--- TheImmature/main.cpp
+++ TheImmature/main.cpp
@@ -1,15 +1,81 @@
-﻿
-
-// Настройки: 
+﻿// Настройки: 
 //linker::system::subsystem  - Windows(/ SUBSYSTEM:WINDOWS) 
-//configuration::advanced::character set - not set 
+//configuration::advanced::character set - use unicode character set 
 //linker::input::additional dependensies Msimg32.lib; Winmm.lib
 
+#include <windows.h>
+#include <iostream>
+
+using namespace std;
+
+struct {
+	int width, height;
+    HWND hwnd;
+	HBITMAP hBack;
 
 
+}window;
 
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR pCmdLine, _In_ int nCmdShow ) {
 
+    const wchar_t* CLASS_NAME = L"Main";  
+    WNDCLASSEX wc = {}; 
 
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.lpfnWndProc = WndProc; 
+    wc.hInstance = hInstance; 
+    wc.hCursor = NULL; 
+    wc.lpszClassName = CLASS_NAME;  
+
+    if (!RegisterClassEx(&wc)) { 
+        MessageBox(NULL, L"Ошибка регистрации класса окна!", L"Ошибка", MB_ICONERROR);
+        return 0;
+    }
+
+    window.width = GetSystemMetrics(SM_CXSCREEN); 
+    window.height = GetSystemMetrics(SM_CYSCREEN);
+
+    window.hwnd = CreateWindowEx(0, CLASS_NAME, L"The Immature", WS_POPUP | WS_MAXIMIZE, 0, 0, window.width, window.height, NULL, NULL, hInstance, NULL);
+
+    if (!window.hwnd) {  
+        MessageBox(NULL, L"Ошибка создания окна!", L"Ошибка", MB_ICONERROR);
+        return 0; 
+
+    }
+
+    ShowWindow(window.hwnd, SW_SHOW);
+    UpdateWindow(window.hwnd);
+
+    MSG msg = {};
+
+    while (GetMessage(&msg, NULL, 0, 0)) {
+
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+
+    }
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+
+    switch (msg) {
+
+    case WM_CREATE:
+        window.hBack = (HBITMAP)LoadImageW(NULL, L"fon.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+
+    case WM_KEYDOWN:
+        if (wParam == VK_ESCAPE) DestroyWindow(window.hwnd); // уничтожаем окно
+
+    case WM_DESTROY:
+
+        PostQuitMessage(0);
+        break;
+
+    default: return DefWindowProc(hwnd, msg, wParam, lParam);
+
+    }
+}
 
 
