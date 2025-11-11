946997efd07d42ef7335c53779aea0fceea1402d
HanaYoriDango <HanaYoriDango@DESKTOP-JFUIT64>
2025-11-11 23:54:12 +0700

    AddCheckFilesInProject

--- TheImmature/main.cpp
+++ TheImmature/main.cpp
@@ -12,9 +12,90 @@ struct {
 	int width, height;
     HWND hwnd;
 	HBITMAP hBack;
+    HDC hdc, mem_dc;
 
 
 }window;
+
+void InitWindow() {
+
+    RECT r;
+    GetClientRect(window.hwnd, &r);
+
+    window.width = r.right - r.left;
+    window.height = r.bottom - r.top;
+
+}
+
+bool FindFiles(const wchar_t* filename) { // ищет файл в проекте
+    // true - найден, false - не найден
+    return GetFileAttributesW(filename) != INVALID_FILE_ATTRIBUTES;
+}
+
+HBITMAP LoadBMP(const wchar_t* name) {
+
+    // сначало проверяем существует ли такой файл в проекте  
+    if (!FindFiles(name)) { 
+        
+        MessageBoxW(NULL, L"Файл в проекте не найден", L"Ошибка", MB_ICONERROR);
+        return NULL;
+    }
+
+    // если он есть, то пробуем загружать файл
+    HBITMAP hBmp = (HBITMAP)LoadImageW(NULL, name, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+
+    // проверяем загрузился ли файл
+    if (!hBmp) MessageBox(NULL, L"Ошибка загрузки файла", L"Ошибка", MB_ICONERROR);
+    return hBmp; // если да, то возвращаем
+
+}
+
+void InitGame() {
+
+    window.hBack = LoadBMP(L"WhileBack.bmp");
+
+
+}
+
+void ShowSprite(int x, int y, int w, int h, HBITMAP hBitmap, bool transparent) {
+
+    BITMAP bm;
+    HDC memDC = CreateCompatibleDC(window.mem_dc);
+    HBITMAP hOldBitmap = (HBITMAP)SelectObject(memDC, hBitmap);
+
+    if (hBitmap) {
+
+        GetObject(hBitmap, sizeof(BITMAP), &bm);
+
+        if (transparent) TransparentBlt(window.mem_dc, x, y, w, h, memDC, 0, 0, w, h, RGB(0, 0, 0));
+        else StretchBlt(window.mem_dc, x, y, w, h, memDC, 0, 0, bm.bmWidth, bm.bmHeight, SRCCOPY);
+
+    }
+
+    SelectObject(memDC, hOldBitmap);
+    DeleteDC(memDC);
+
+}
+
+void ShowObject() {
+
+    ShowSprite(0, 0, window.width, window.height, window.hBack, false);
+
+}
+void ShowGame() {
+
+    window.mem_dc = CreateCompatibleDC(window.hdc);
+    HBITMAP hMemBmp = CreateCompatibleBitmap(window.hdc, window.width, window.height);
+    HBITMAP hOldBmp = (HBITMAP)SelectObject(window.mem_dc, hMemBmp);
+
+    ShowObject();
+
+    BitBlt(window.hdc, 0, 0, window.width, window.height, window.mem_dc, 0, 0, SRCCOPY);
+
+    SelectObject(window.mem_dc, hOldBmp);
+    DeleteObject(hOldBmp);
+    DeleteDC(window.mem_dc);
+}
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
@@ -45,6 +126,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 
     }
 
+    InitWindow();
+
     ShowWindow(window.hwnd, SW_SHOW);
     UpdateWindow(window.hwnd);
 
@@ -63,10 +146,24 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
 
     case WM_CREATE:
-        window.hBack = (HBITMAP)LoadImageW(NULL, L"fon.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+        InitGame();
+
+        break;
+    
+    case WM_PAINT: 
+    {
+        PAINTSTRUCT ps;
+        window.hdc = BeginPaint(hwnd, &ps);
+        ShowGame();
+        EndPaint(hwnd, &ps);
+
+        break;
+    }
 
     case WM_KEYDOWN:
         if (wParam == VK_ESCAPE) DestroyWindow(window.hwnd); // уничтожаем окно
+
+        break;
 
     case WM_DESTROY:
 
